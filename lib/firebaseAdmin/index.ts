f59a02fa4bddfbb55b53d46867d89d213128fd35
skyteck-belgium <skyteck.belgium@gmail.com>
2025-05-21 14:19:30 +0200

    Update and rename hbj to index.ts

---
+++ lib/firebaseAdmin/index.ts
@@ -0,0 +1,27 @@
+import { initializeApp, cert, getApps } from 'firebase-admin/app'
+import { getAuth } from 'firebase-admin/auth'
+import serviceAccount from './serviceAccountKey.json'
+
+if (!getApps().length) {
+  if (
+    process.env.FIREBASE_PROJECT_ID &&
+    process.env.FIREBASE_CLIENT_EMAIL &&
+    process.env.FIREBASE_PRIVATE_KEY
+  ) {
+    // In productie: init met env vars
+    initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      }),
+    })
+  } else {
+    // Lokaal: init met serviceAccountKey.json
+    initializeApp({
+      credential: cert(serviceAccount),
+    })
+  }
+}
+
+export const auth = getAuth()
