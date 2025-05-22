81588f0009cbb4fbe0ccd6fed56e90bd8c00afff
skyteck-belgium <skyteck.belgium@gmail.com>
2025-05-22 20:30:12 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from 'next'
+
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // je config opties hier
-}
+  /* config options here */
+};
 
-export default nextConfig
+export default nextConfig;
