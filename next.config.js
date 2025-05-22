a3e65c00e5d25e90a9e59c65ddb31e29badcc097
skyteck-belgium <skyteck.belgium@gmail.com>
2025-05-22 20:22:14 +0200

    Update and rename next.config.js to next.config.ts

--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // hier kun je je config opties zetten
-};
-
-module.exports = nextConfig;
---
+++ next.config.ts
@@ -0,0 +1,7 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  // je config opties hier
+}
+
+export default nextConfig
