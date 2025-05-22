e2d3e17005fbfed41ef322cdc1c8d0d6b85c035f
skyteck-belgium <skyteck.belgium@gmail.com>
2025-05-22 20:14:09 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // hier kun je je config opties zetten
 };
 
-export default nextConfig;
+module.exports = nextConfig;
