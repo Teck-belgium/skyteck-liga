36110155ede344c2143c7f838cd62094b9a6bd45
Teck-belgium <skyteck.belgium@gmail.com>
2025-07-07 18:39:13 +0200

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -160,7 +160,7 @@ useEffect(() => {
           <p className="font-medium pt-4">🏷️ Clubs</p>
           <div className="flex flex-wrap gap-4">
             {clubOptions.map((club) => (
-              <label key={club.} className="flex items-center gap-1">
+              <label key={club} className="flex items-center gap-1">
                 <input
                   type="checkbox"
                   checked={selectedClubs.includes(club)}
